546cc13860fd5a0199b001f6b56f470c136bcb3c
Serge Nevsky <sergey@sergeyk.com>
2015-10-26 23:06:09 -0500

    Add navigation between views.

--- www/js/app.js
+++ www/js/app.js
@@ -5,23 +5,27 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'home.html'
+  })
+  $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'add.html'
+  })
+
+  $urlRouterProvider.otherwise('/')
 })
 
 .controller('TodoCtrl', function($scope) {
-    $scope.tasks = [
-      { title: 'First task' },
-      { title: 'Second task' },
-      { title: 'Third task' }
-    ];
-  });
+  $scope.tasks = [
+    { title: 'First task' },
+    { title: 'Second task' },
+    { title: 'Third task' }
+  ];
+})
+
+.controller('AddNewTaskCtrl', function($scope) {
+  $scope.message = 'Add new task here';
+});
