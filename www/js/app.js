854370691ffc5922d94db1bdbe89bae758b3db5f
Serge Nevsky <sergey@sergeyk.com>
2015-10-27 22:54:55 -0500

    Style the iOS status bar.

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,17 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
 
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault(1);
+      }
+    });
+  })
+
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
       url: '/',
